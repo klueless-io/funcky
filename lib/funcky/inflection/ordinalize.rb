4884c656de4edfb3f7394d9d8db01dcbb3e665ec
David Cruwys <david@ideasmen.com.au>
2021-10-06 17:52:17 +1100

    fix cop

--- lib/funcky/inflection/ordinalize.rb
+++ lib/funcky/inflection/ordinalize.rb
@@ -1,4 +1,4 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
 # # reference: https://github.com/rails/rails/blob/master/activesupport/lib/active_support/inflector/methods.rb
 # # require 'active_support/inflector/inflections'
